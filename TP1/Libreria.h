2d50ff38124a8d7a3c101d355147ae70723d2bbe
adrianlopezdasilva <40776599+adrianlopezdasilva@users.noreply.github.com>
2020-10-26 19:18:39 -0300

    Rename Libreria.h to TP1/Libreria.h

---
+++ TP1/Libreria.h
@@ -0,0 +1,10 @@
+#ifndef LIBRERIA_H_INCLUDED
+#define LIBRERIA_H_INCLUDED
+
+float suma(float a, float b);
+float resta(float a, float b);
+float multiplicacion(float a, float b);
+float division(float a, float b);
+int factorial(int a);
+
+#endif // LIBRERIA_H_INCLUDED
